e9cc3570f69943b9a06097cdffadb15e592e5808
Your Name <you@example.com>
2025-02-12 19:08:53 +0000

    docker deploy stops the prev container

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -2,4 +2,5 @@
 
 git pull
 docker build -t akiba .
+docker stop $(docker ps | awk '$2=="akiba" {print $NF}')
 docker run -d -p 3001:3001 --restart=always --mount type=volume,src=akiba,dst=/usr/src/app/public akiba
